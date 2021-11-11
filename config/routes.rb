2f987e620da9bb5df586f97a321c15c560ff7e66
makikoTakemori <richikaemay@gmail.com>
2021-11-11 08:14:01 +0000

    [add]4-9

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'top' => 'homes#top'
+  post 'books' => 'books#create'
+  get 'books' => 'books#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
